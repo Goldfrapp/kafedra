0eb204cf5f34ce4c9effc263fd14fe7bcac370b5
Goldfrapp <andreysnu@gmail.com>
2021-06-11 12:12:11 +0300

    Fix Category Model in Blog App

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,12 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=160, verbose_name="Название")
     url = models.SlugField(max_length=255, unique=True, verbose_name="Ссылка")
+    description = models.CharField(max_length=255, blank=True, verbose_name="Описание категории")
+    image = models.ImageField(upload_to='blog/category', blank=True, verbose_name="Изображение")
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = "Категории"
 
     def __str__(self):
         return self.title
